9274fed32fddbbd7367362f9cf8f412eb56dce38
Rainer Hihn <hihn.rainer@gmail.com>
2020-07-19 13:46:57 +0200

    Fixed title test

--- angularclient/src/app/app.component.spec.ts
+++ angularclient/src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ampd app is running!');
+    expect(compiled.querySelector('#ampd-logo').textContent).toContain('ampd');
   });
 });
