cc8d36b73eb525f080d02f36947d8a2e76cd6137
Rainer Hihn <hihn.rainer@gmail.com>
2021-04-08 17:56:07 +0200

    update

---
+++ src/test/java/org/hihn/ampd/server/service/BrowseServiceTest.java
@@ -0,0 +1,15 @@
+package org.hihn.ampd.server.service;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BrowseServiceTest {
+
+}
