d763bd4c7d91496fbd7c26d9d2dcc71ad316ec9f
Rainer Hihn <hihn.rainer@gmail.com>
2021-05-27 17:42:22 +0200

    M angularclient/src/app/genres/genres.component.html  M angularclient/src/app/genres/genres.component.scss  M angularclient/src/app/genres/genres.component.ts  M angularclient/src/app/shared/services/genres.service.ts  M angularclient/src/environments/environment.ts

--- angularclient/src/app/genres/genres.component.ts
+++ angularclient/src/app/genres/genres.component.ts
@@ -1,8 +1,11 @@
-import {Component} from "@angular/core";
-import {GenresService} from "../shared/services/genres.service";
-import {Observable, of} from "rxjs";
-import {ErrorMsg} from "../shared/error/error-msg";
-import {MatChip} from "@angular/material/chips";
+import { Component } from "@angular/core";
+import { GenresService } from "../shared/services/genres.service";
+import { Observable, of } from "rxjs";
+import { ErrorMsg } from "../shared/error/error-msg";
+import { TrackTableData } from "../shared/track-table/track-table-data";
+import { ClickActions } from "../shared/track-table/click-actions.enum";
+import { MatTableDataSource } from "@angular/material/table";
+import { QueueTrack } from "../shared/models/queue-track";
 
 @Component({
   selector: "app-genres",
@@ -10,23 +13,58 @@ import {MatChip} from "@angular/material/chips";
   styleUrls: ["./genres.component.scss"],
 })
 export class GenresComponent {
+  dataSource = new MatTableDataSource<QueueTrack>([]);
   error: ErrorMsg | null = null;
-  genres: Observable<String[]> | null = null;
-  selected = new Set();
+  genres: Observable<string[]> | null = null;
+  isMobile = false;
+  trackTableData = new TrackTableData();
 
   constructor(private genresService: GenresService) {
     genresService.listAll().subscribe(
-        (genres: String[]) => {
-          this.genres = of(genres);
-        },
-        (err: ErrorMsg) => {
-          this.error = err;
-        }
+      (genres: string[]) => {
+        this.genres = of(genres);
+      },
+      (err: ErrorMsg) => {
+        this.error = err;
+      }
     );
   }
 
-  onChange(genre: String) {
-    console.log("genre", genre)
-    this.selected.add(genre);
+  onChange(genre: string) {
+    this.genresService.getWords(genre).subscribe((tracks) => {
+      const tableData = tracks.map(
+        (track, index) => new QueueTrack(track, index)
+      );
+      this.dataSource = new MatTableDataSource<QueueTrack>(tableData);
+      this.trackTableData = this.buildTableData();
+    });
+  }
+
+  private buildTableData(): TrackTableData {
+    const trackTable = new TrackTableData();
+    trackTable.addTitleColumn = true;
+    trackTable.clickable = true;
+    trackTable.dataSource = this.dataSource;
+    trackTable.displayedColumns = this.getDisplayedColumns();
+    trackTable.onPlayClick = ClickActions.AddPlayTrack;
+    trackTable.notify = true;
+    trackTable.pagination = true;
+    trackTable.playTitleColumn = true;
+    trackTable.sortable = true;
+    return trackTable;
+  }
+
+  private getDisplayedColumns(): string[] {
+    const displayedColumns = [
+      { name: "artistName", showMobile: true },
+      { name: "albumName", showMobile: false },
+      { name: "title", showMobile: true },
+      { name: "length", showMobile: false },
+      { name: "playTitle", showMobile: true },
+      { name: "addTitle", showMobile: true },
+    ];
+    return displayedColumns
+      .filter((cd) => !this.isMobile || cd.showMobile)
+      .map((cd) => cd.name);
   }
 }
--- angularclient/src/app/shared/services/genres.service.ts
+++ angularclient/src/app/shared/services/genres.service.ts
@@ -4,22 +4,37 @@ import { SettingsService } from "./settings.service";
 import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 import { ErrorMsg } from "../error/error-msg";
+import { Track } from "../messages/incoming/track";
 
 @Injectable({
   providedIn: "root",
 })
 export class GenresService {
+  private apiUrl: string;
+
   constructor(
     private http: HttpClient,
     private settingsService: SettingsService
-  ) {}
+  ) {
+    this.apiUrl = `${this.settingsService.getBackendContextAddr()}api/genres/`;
+  }
 
-  listAll(): Observable<String[]> {
-    const url = `${this.settingsService.getBackendContextAddr()}api/genres/`;
-    return this.http.get<String[]>(url).pipe(
+  listAll(): Observable<string[]> {
+    return this.http.get<string[]>(this.apiUrl).pipe(
       catchError((err: HttpErrorResponse) =>
         throwError({
           title: `Got an error while retrieving genres`,
+          detail: err.message,
+        } as ErrorMsg)
+      )
+    );
+  }
+
+  getWords(genre: string): Observable<Track[]> {
+    return this.http.get<Track[]>(`${this.apiUrl}/filter?genre=${genre}`).pipe(
+      catchError((err: HttpErrorResponse) =>
+        throwError({
+          title: `Got an error while retrieving tracks for genre: ${genre}`,
           detail: err.message,
         } as ErrorMsg)
       )
